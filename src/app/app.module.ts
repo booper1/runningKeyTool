ed0679998e6f9ca22946d9d7e8f526b383bf07c6
Cooper Baumgarth <cooperbaumgarth@gmail.com>
2022-11-11 02:02:16 -0500

    Initial setup of header, content, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        ContentComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FontAwesomeModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+    constructor(library: FaIconLibrary) {
+        library.addIconPacks(fas, far);
+    }
+}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Global } from '../global';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+    title = Global.title;
+    navItems = [
+        { title: "More", route: "#" },
+        { title: "Coming", route: "#" },
+        { title: "Soon", route: "#" },
+        { title: "™", route: "#" }
+    ];
+    shuffleColor: string = "#333";
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    onClickSettings(): void {
+        //console.log("Toggle Settings Menu");
+    }
+
+    onClickShuffle(): void {
+        console.log("Shuffle");
+         this.shuffleColor = '#' + (Math.random() * 0xCCCCCC << 0).toString(16).padStart(6, '0');
+    }
+
+}
