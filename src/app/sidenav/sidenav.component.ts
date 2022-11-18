6cc7c22ab8966f954f28bc73723b609d0c346f5e
Cooper Baumgarth <cooperbaumgarth@gmail.com>
2022-11-18 15:41:36 -0500

    Updated sidebar nav with more responsivenes

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
@@ -8,20 +9,48 @@ import { IconProp } from '@fortawesome/fontawesome-svg-core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
-    navItems: { title: string; route: string; icon: IconProp }[] = [
-        { title: "Home", route: "", icon: "house"},
-        { title: "About", route: "about", icon: "user"},
-        { title: "Projects", route: "projects", icon: "list-check"},
-        { title: "Contact", route: "contact", icon: "message"}
+    navItems: { title: string; icon: IconProp }[] = [
+        { title: "home", icon: "house"},
+        { title: "about", icon: "user"},
+        { title: "projects", icon: "list-check"},
+        { title: "contact", icon: "message"}
     ];
+    
+    topBun: any;
+    patty: any;
+    botBun: any;
+    sidebar: any;
+    nav: any;
+
+    constructor(private router: Router,
+                private route: ActivatedRoute) {
+    }
 
     ngOnInit(): void {
-        let length = 0;
+        let maxLength = 0;
         for (let i: number = 0; i < this.navItems.length; i++) {
-            if (this.navItems[i].title.length > length) {
-                length = this.navItems[i].title.length;
+            if (this.navItems[i].title.length > maxLength) {
+                maxLength = this.navItems[i].title.length;
             }
         }
-        document.documentElement.style.setProperty("--nav-length", (length + 6).toString() + "ch");
+        document.documentElement.style.setProperty("--nav-length", (maxLength + 6).toString() + "ch");
+
+        this.topBun = document.getElementById("b1");
+        this.patty = document.getElementById("b2");
+        this.botBun = document.getElementById("b3");
+        this.sidebar = document.getElementById("side");
+        this.nav = document.getElementById("nav");
+    }
+
+    onKeyDownEvent(route: string) {
+        this.router.navigate([route], { relativeTo: this.route });
+    }
+
+    toggle(): void {
+        this.topBun != null ? this.topBun.classList.toggle('angle') : console.log("Undefined");
+        this.patty != null ? this.patty.classList.toggle('hide') : console.log("Undefined");
+        this.botBun != null ? this.botBun.classList.toggle('angle') : console.log("Undefined");
+        this.sidebar != null ? this.sidebar.classList.toggle('show') : console.log("Undefined");
+        this.nav != null ? this.nav.classList.toggle('show') : console.log("Undefined");
     }
 }
