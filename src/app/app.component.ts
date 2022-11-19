10af82f5e53c51c32b03c377e903f675a2b53dd0
Cooper Baumgarth <cooperbaumgarth@gmail.com>
2022-11-18 23:37:30 -0500

    Improved navbar styling, added better responsiveness for mobile support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Global } from './global';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    ngOnInit(): void {
+        Global.sidebar = document.getElementById("side");
+        Global.clickNet = document.getElementById("clickNet");
+        Global.nav = document.getElementById("nav");
+        Global.main = document.getElementById("main");
+    }
+
+    toggle(): void {
+        Global.toggle();
+    }
 }
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -2,6 +2,7 @@ import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { Global } from '../global';
 
 @Component({
   selector: 'app-sidenav',
@@ -15,31 +16,19 @@ export class SidenavComponent implements OnInit {
         { title: "projects", icon: "list-check"},
         { title: "contact", icon: "message"}
     ];
-    
-    topBun: any;
-    patty: any;
-    botBun: any;
-    sidebar: any;
-    nav: any;
 
     constructor(private router: Router,
                 private route: ActivatedRoute) {
     }
 
     ngOnInit(): void {
-        let maxLength = 0;
-        for (let i: number = 0; i < this.navItems.length; i++) {
-            if (this.navItems[i].title.length > maxLength) {
-                maxLength = this.navItems[i].title.length;
-            }
-        }
-        document.documentElement.style.setProperty("--nav-length", (maxLength + 6).toString() + "ch");
-
-        this.topBun = document.getElementById("b1");
-        this.patty = document.getElementById("b2");
-        this.botBun = document.getElementById("b3");
-        this.sidebar = document.getElementById("side");
-        this.nav = document.getElementById("nav");
+        Global.topBun = document.getElementById("b1");
+        Global.patty = document.getElementById("b2");
+        Global.botBun = document.getElementById("b3");
+        Global.sidebar = document.getElementById("side");
+        Global.clickNet = document.getElementById("clickNet");
+        Global.nav = document.getElementById("nav");
+        Global.main = document.getElementById("main");
     }
 
     onKeyDownEvent(route: string) {
@@ -47,10 +36,6 @@ export class SidenavComponent implements OnInit {
     }
 
     toggle(): void {
-        this.topBun != null ? this.topBun.classList.toggle('angle') : console.log("Undefined");
-        this.patty != null ? this.patty.classList.toggle('hide') : console.log("Undefined");
-        this.botBun != null ? this.botBun.classList.toggle('angle') : console.log("Undefined");
-        this.sidebar != null ? this.sidebar.classList.toggle('show') : console.log("Undefined");
-        this.nav != null ? this.nav.classList.toggle('show') : console.log("Undefined");
+        Global.toggle();
     }
 }
