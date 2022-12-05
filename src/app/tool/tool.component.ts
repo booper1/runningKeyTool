9a7f76a93d5d7c1787cf838d149ff95219c1e1c7
Cooper Baumgarth <cooperbaumgarth@gmail.com>
2022-12-05 00:01:28 -0500

    Added more functionality, analysis, shifting, etc.

--- src/app/tool/tool.component.ts
+++ src/app/tool/tool.component.ts
@@ -7,7 +7,13 @@ import { Global } from '../global';
     styleUrls: ['./tool.component.scss']
 })
 export class ToolComponent {
+    ciphertext: string = "";
+    crib: string = "";
     output: string = "";
+    baseIndex: number = 0;
+    buffer: string = "";
+    error: boolean = false;
+    errorMsg: string = "Please enter only letter characters";
 
     ngOnInit(): void {
         Global.light = document.getElementById("light");
@@ -32,14 +38,51 @@ export class ToolComponent {
     }
 
     analyze(): void {
-        this.output = ((document.getElementById("ciphertext") as HTMLInputElement).value) + " ; " + ((document.getElementById("crib") as HTMLInputElement).value);
-        ((document.getElementById("ciphertext") as HTMLInputElement).value) = "";
-        ((document.getElementById("crib") as HTMLInputElement).value) = "";
+        this.ciphertext = ((document.getElementById("ciphertext") as HTMLInputElement).value).toLowerCase().replace(/\s/g, "");
+        this.crib = ((document.getElementById("crib") as HTMLInputElement).value).toLowerCase().replace(/\s/g, "");
+
+        if (/^[A-Za-z\s]*$/.test(this.ciphertext) && /^[A-Za-z\s]*$/.test(this.crib)) {
+            this.error = false;
+            this.output = "";
+
+            for (let i: number = this.baseIndex; i < this.ciphertext.length && i < this.crib.length + this.baseIndex; i++) {
+                let index = ((this.ciphertext.charAt(i).charCodeAt(0) - 96) - (this.crib.charAt(i - this.baseIndex).charCodeAt(0) - 96));
+                let charCode = (((index % 26) + 26) % 26) + 1;
+                this.output += String.fromCharCode(charCode + 96);
+            }
+
+            this.buffer = "";
+            for (let i: number = 0; i < this.baseIndex; i++) {
+                this.buffer += " ";
+            }
+            this.ciphertext = this.ciphertext.toUpperCase();
+        } else {
+            this.ciphertext = "";
+            this.crib = "";
+            this.output = "";
+            this.error = true;
+        }
     }
 
     clear(): void {
         ((document.getElementById("ciphertext") as HTMLInputElement).value) = "";
         ((document.getElementById("crib") as HTMLInputElement).value) = "";
+        this.ciphertext = "";
+        this.crib = "";
         this.output = "";
+    }
+
+    shift(direction: number): void {
+        if (direction === 0) { // Shift Left
+            if (this.baseIndex > 0) {
+                this.baseIndex--;
+                this.analyze();
+            }
+        } else { // Shift Right
+            if (this.baseIndex < this.ciphertext.length - this.crib.length) {
+                this.baseIndex++;
+                this.analyze();
+            }
+        }
     }
 }
