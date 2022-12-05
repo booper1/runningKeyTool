95ded8303aef980121603bc477ab4bc7ebd1bd24
Cooper Baumgarth <cooperbaumgarth@gmail.com>
2022-12-04 20:38:07 -0500

    Follow up attempt to fix GHPages resource loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         FontAwesomeModule,
         BrowserAnimationsModule
     ],
-    providers: [],
+    providers: [{ provide: APP_BASE_HREF, useValue: '/runningKeyTool' }],
     bootstrap: [AppComponent]
 })
 export class AppModule {
